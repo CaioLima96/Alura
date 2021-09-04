e694becf94cbf45fea87430da5b5f9b078c63938
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-09-04 00:56:37 -0300

    aula 04 - atividade 04 feita - js web manipule dom com js

--- js-na-web-manipule-o-dom-com-javascript/assets/js/listaDeTarefas.js
+++ js-na-web-manipule-o-dom-com-javascript/assets/js/listaDeTarefas.js
@@ -11,15 +11,30 @@ const criarTarefa = (evento) => {
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
     const conteudo = `<p class="content">${valor}</p>`
-
     tarefa.innerHTML = conteudo
 
+    tarefa.appendChild(BotaoConclui())
     lista.appendChild(tarefa)
-
     input.value = ' '
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConclui = () => {
+    const BotaoConclui = document.createElement('button')
+    BotaoConclui.classList.add('check-button')
+    BotaoConclui.innerText = 'concluir'
+
+    BotaoConclui.addEventListener('click', concluirTarefa)
+
+    return BotaoConclui
+}
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}
